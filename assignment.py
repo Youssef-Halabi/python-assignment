f71cb3904fb8d05ecc79a796e019be9d2123eaa1
Youssef-Halabi <youssefhalabi174@gmail.com>
2025-10-05 01:48:07 +0300

    wrote a code

--- assignment.py
+++ assignment.py
@@ -7,3 +7,26 @@ while True:
     print("2.Deposit Money")
     print("3.Withdraw Money")
     print("4.Exit")
+
+    choose = int(input("Choose one of these four options: "))
+    if choose == 1:
+        print(Balance)
+    elif choose == 2:
+        cash = int(input("Enter the amount of deposit: "))
+        Balance = Balance + cash
+        print("Deposit successful. New balance is: ", Balance)
+    elif choose == 3:
+        withdraw = int(input("Enter amount to withdraw: "))
+        if withdraw >= Balance:
+            print("Insufficient funds!")
+        elif withdraw < Balance:
+            Balance = Balance - withdraw 
+            print("Withdrawal successful. New balance = ", Balance)
+    else:
+        print("Goodbye!")
+
+
+
+
+        
+
