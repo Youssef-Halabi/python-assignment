f32732670a4547588e85529b45fe0106b169a8f5
Youssef-Halabi <youssefhalabi174@gmail.com>
2025-10-05 01:05:18 +0300

    First commit

---
+++ assignment.py
@@ -0,0 +1,9 @@
+Balance = 1000
+print("Welcome to the ATM")
+print("Your balance is: ", Balance)
+
+while True:
+    print("1.Check Balance")
+    print("2.Deposit Money")
+    print("3.Withdraw Money")
+    print("4.Exit")
